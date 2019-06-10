0f64309e408d236664372e9f5e38f13be2924d59
Dinar Hasanshin <hudazi0158@gmail.com>
2019-06-10 22:50:36 +0300

    slider

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop: true,
+  	items: 5,
+  	dots: false,
+  	autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+  });
+});
